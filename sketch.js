288585e22219b58acd74767cc9f103803bac8646
mikima <mikimauri@gmail.com>
2017-01-24 15:43:33 +0100

    t

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
-var value = 0;
+var energy = 0;
 var singleShake = 0;
+
+var level1 = 20000;
 
 //var value2 = 0;
 
@@ -11,14 +13,27 @@ function setup() {
 }
 
 function draw() {
+  if (energy >= level1) {
+    //display things
+    background(0);
+  } else {
+    background(204);
+  }
 
-   text(value, width/2, height/2);
-   text(singleShake, width/2, height/2+40);
+  text(energy, width/2, height/2);
+  text(singleShake, width/2, height/2+40);
+
+
 }
 
-  
-// 2)
+
+function reset() {
+
+  //set all the variables at original value
+  energy = 0;
+}
+
 function deviceShaken(){
   singleShake = abs(accelerationX) + abs(accelerationY) + abs(accelerationZ);
-  value += singleShake;
+  energy += singleShake;
 }
