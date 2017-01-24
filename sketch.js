59bf60538e83949a250d70674ca079911dbe4a5c
mikima <mikimauri@gmail.com>
2017-01-24 15:27:35 +0100

    added background check

--- sketch.js
+++ sketch.js
@@ -1,13 +1,21 @@
 var value = 0;
 var singleShake = 0;
+
+var finished = false;
 //var value2 = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  setShakeThreshold(0);
 }
 
 function draw() {
-   background(204);
+   if(finished == false) {
+    background(204);
+   } else {
+    background(255,0,0);
+   }
+
    text(value, width/2, height/2);
    text(singleShake, width/2, height/2+40);
 }
@@ -15,6 +23,12 @@ function draw() {
   
 // 2)
 function deviceShaken(){
-    value += abs(accelerationX) + abs(accelerationY) + abs(accelerationZ);
     singleShake = abs(accelerationX) + abs(accelerationY) + abs(accelerationZ);
+
+    if(singleShake > 20){
+        value += abs(accelerationX) + abs(accelerationY) + abs(accelerationZ);
+    } else {
+        finished = true;
+    }
+
 }
