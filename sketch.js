1637a297d9cc7b0780cf0e5b01435feb138a0929
mikima <mikimauri@gmail.com>
2017-01-24 15:10:29 +0100

    updated the simple branch

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var value = 0;
+var singleShake = 0;
 //var value2 = 0;
 
 function setup() {
@@ -7,67 +8,13 @@ function setup() {
 
 function draw() {
    background(204);
-    
-    textSize(height/20);
-    textAlign(CENTER);
-    textStyle(BOLD);
-    fill(0);
-    noStroke();
-    text("SHAKE YOUR DEVICE", width/2, height - height/1.1);   
-    textSize(height/50);
-    text("value", width/2, height - height/11);
-  
-     textSize(height/40);
-     textAlign(CENTER);
-     textStyle(NORMAL);
-     text(value, width/2, height - height/8);
-  /*
-    textSize(height/30);
-    textAlign(CENTER);
-    textStyle(NORMAL);
-    text(value, width/2, height - height/8);
-    //var valore = deviceShaken(value);                     //deviceShaken(); chiamarlo in draw() mi fa cambiare value in continuazione...
-  
-   /* 1) for (valore > 0; valore < 1; valore += valore){      2) for (value > 0; value < 1; value += value){
-
-        textSize(height/30);                                    textSize(height/30);
-        textAlign(CENTER);                                      textAlign(CENTER);
-        textStyle(NORMAL);                                      textStyle(NORMAL);
-        
-        text(valore, width/2, height - height/8);               text(valore, width/2, height - height/8);
-     
-    }                                                            }*/
-    //deviceShaken(); 
-   //for (value > 0; value < 1; value += value){
-   /* var i = [];
-    for (i > 1; i < 100; i++){
-     value = (abs(accelerationX) + abs(accelerationY) + abs(accelerationZ)) + (abs(pAccelerationX) + abs(pAccelerationY) + abs(pAccelerationZ));
-     textSize(height/40);
-     textAlign(CENTER);
-     textStyle(NORMAL);
-     text(value, width/2, height - height/8);
-     
-   }
-/* 1) function deviceShaken(val){
-    val = abs(accelerationX) + abs(accelerationY) + abs(accelerationZ);
-    return val;
-}*/
+   text(value, width/2, height/2);
+   text(singleShake, width/2, height/2+40);
 }
-  
+
   
 // 2)
 function deviceShaken(){
-    value = abs(accelerationX) + abs(accelerationY) + abs(accelerationZ);
-    for (value = 0; value < 100; value += value){
-     value = (abs(accelerationX) + abs(accelerationY) + abs(accelerationZ)) + (abs(pAccelerationX) + abs(pAccelerationY) + abs(pAccelerationZ));
-    // textSize(height/40);
-    // textAlign(CENTER);
-    // textStyle(NORMAL);
-    // text(value, width/2, height - height/8);
-     
-   }
-  
- //function deviceShaken(){ 
- // value = abs(accelerationX) + abs(accelerationY) + abs(accelerationZ);
-    //value2 = abs(pAccelerationX) + abs(pAccelerationY) + abs(pAccelerationZ);
-//}
+    value += abs(accelerationX) + abs(accelerationY) + abs(accelerationZ);
+    singleShake = abs(accelerationX) + abs(accelerationY) + abs(accelerationZ);
+}
